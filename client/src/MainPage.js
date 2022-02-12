5c7b985d40a6d6e6f4ebde487a4e5e5b2877a72b
witch-factory <soakdma37@gmail.com>
2022-02-12 23:40:42 +0900

    드롭다운 메뉴 뼈대 구현

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,14 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const HeadPrimaryLogo = function HeadPrimaryLogo() {
+const HeaderMainLogo = styled.h1`
+  font-size:3rem;
+  margin: 3px;
+`;
+
+const HeaderSubLogo = styled.h2`
+  font-size:1.5rem;
+  margin:0;
+`;
+
+function HeaderLogo() {
   return (
-    <h1>메인 로고</h1>
+    <>
+      <HeaderMainLogo>Reviewary</HeaderMainLogo>
+      <HeaderSubLogo>너와 나의 리뷰 도서관</HeaderSubLogo>
+    </>
   );
-};
+}
+
+const DropDownMenuContainer = styled.div`
+  color:#000000;
+  width:10rem;
+  margin: 0 auto;
+`;
+
+const DropDownMenuHeader = styled.div`
+  margin-bottom: 0.8rem;
+  font-weight: 500;
+  font-size: 1.3rem;
+  background: #ffffff;
+`;
+
+const DropDownMenuListContainer = styled.div``;
+
+const DropDownMenuList = styled.ul`
+  padding: 0;
+  margin: 0;
+  background: #ffffff;
+  border:2px solid #000000;
+  box-sizing: border-box;
+  color:#000000;
+  font-size: 1.3rem;
+  font-weight: 500;
+  &:first-child{
+    padding-top: 0.8rem;
+  }
+`;
+
+const DropDownListItem = styled.li`
+  list-style: none;
+  margin-bottom: 0.8rem;
+`;
+
+const firstMenuDropDown = [
+  '자유게시판',
+  '리뷰게시판',
+  '장르별게시판',
+];
 
 function MainPage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const menuToggle = () => setIsOpen(!isOpen);
+  const onOptionSelected = (value) => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    // eslint-disable-next-line no-console
+    console.log(selectedOption);
+  };
+
   return (
-    <HeadPrimaryLogo />
+    <>
+      <HeaderLogo />
+      <DropDownMenuListContainer>
+        <DropDownMenuHeader onClick={menuToggle}>전체글</DropDownMenuHeader>
+        {isOpen && (
+          <DropDownMenuContainer>
+            <DropDownMenuList>
+              {firstMenuDropDown.map((item) => (
+                <DropDownListItem
+                  onClick={onOptionSelected(item)}
+                  key={item}
+                >
+                  {item}
+                </DropDownListItem>
+              ))}
+            </DropDownMenuList>
+          </DropDownMenuContainer>
+        )}
+      </DropDownMenuListContainer>
+    </>
   );
 }
 
