b54161fbc22298a4ca91f8b946198abf6a9f7d43
witch-factory <soakdma37@gmail.com>
2022-02-16 02:37:45 +0900

    검색창 컴포넌트 파일 분리

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
 import HeaderLogo from './common/HeaderLogo';
 import HorizontalContainer from './common/HorizontalContainer';
 import DropDownMenu from './common/DropDownMenu';
@@ -10,6 +8,7 @@ import book3 from './image/book3.jpg';
 import book4 from './image/book4.jpg';
 import book5 from './image/book5.jpg';
 import SlideMenu from './common/SlideMenu';
+import SearchBox from './common/SearchBox';
 import VerticalContainer from './common/VerticalContainer';
 
 const firstMenuDropDown = [
@@ -31,43 +30,6 @@ const thirdMenuDropDown = [
 ];
 
 const slideMenuImageList = [book1, book2, book3, book4, book5];
-
-const SearchBoxContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin: 1rem 0;
-`;
-
-const SearchTextBox = styled.input`
-  width: 15rem;
-  border-radius: 15rem;
-  padding-left:1rem;
-  height: 2rem;
-  font-size: 1.5rem;
-`;
-
-const SearchButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-function SearchBoxSearchButton() {
-  return (
-    <SearchButtonContainer>
-      <MdSearch size="30" />
-    </SearchButtonContainer>
-  );
-}
-
-function SearchBox() {
-  return (
-    <SearchBoxContainer>
-      <SearchTextBox />
-      <SearchBoxSearchButton />
-    </SearchBoxContainer>
-  );
-}
 
 function MainPage() {
   return (
