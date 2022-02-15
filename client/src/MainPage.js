3bb7cd187488f15eceb956be7636b9436aa51f81
witch-factory <soakdma37@gmail.com>
2022-02-16 01:07:44 +0900

    캐로셀 완성

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -6,6 +6,7 @@ import book1 from './image/book1.jpg';
 import book2 from './image/book2.jpg';
 import book3 from './image/book3.jpg';
 import book4 from './image/book4.jpg';
+import book5 from './image/book5.jpg';
 import SlideMenu from './common/SlideMenu';
 import VerticalContainer from './common/VerticalContainer';
 
@@ -27,7 +28,7 @@ const thirdMenuDropDown = [
   '장르별게시판',
 ];
 
-const slideMenuImageList = [book1, book2, book3, book4];
+const slideMenuImageList = [book1, book2, book3, book4, book5];
 
 function MainPage() {
   return (
--- client/src/common/SlideMenu.js
+++ client/src/common/SlideMenu.js
@@ -4,23 +4,32 @@ import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md';
 import book1 from '../image/book1.jpg';
 
 const SlideImage = styled.img`
-  width:100%;
-  height:50vh;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
+
+const SlideImageContainer = styled.div`
+  flex:1 0 auto;
+  width: 100%;
 `;
 
 function SlideItem({ img }) {
   return (
-    <SlideImage src={img || book1} />
+    <SlideImageContainer>
+      <SlideImage src={img || book1} />
+    </SlideImageContainer>
   );
 }
 
-const SlideContainer = styled.div`
+const SlideItemContainer = styled.div`
   width: 100%;
+  height:500px;
   display: flex; //이미지들을 가로로 나열
 `;
 
-const SlideItemContainer = styled.div`
-  width:50%;
+const SlideContainer = styled.div`
+  width:600px;
   overflow: hidden;
 `;
 
@@ -61,13 +70,13 @@ function SlideMenu({ slideMenuImages }) {
   return (
     <SlideMenuContainer>
       <MdArrowBackIosNew size="30" onClick={prevSlide} />
-      <SlideItemContainer>
-        <SlideContainer ref={slideRef}>
+      <SlideContainer>
+        <SlideItemContainer ref={slideRef}>
           {slideMenuImages.map((item) => (
             <SlideItem img={item} />
           ))}
-        </SlideContainer>
-      </SlideItemContainer>
+        </SlideItemContainer>
+      </SlideContainer>
       <MdArrowForwardIos size="30" onClick={nextSlide} />
     </SlideMenuContainer>
   );
