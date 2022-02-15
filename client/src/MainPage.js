d39d432214015b321860dae4a40ca0fe683680a1
witch-factory <soakdma37@gmail.com>
2022-02-15 18:01:07 +0900

    캐로셀 컴포넌트 파일 분리

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
-import { MdArrowBackIosNew, MdArrowForwardIos } from 'react-icons/md';
+import React from 'react';
 import HeaderLogo from './common/HeaderLogo';
 import HorizontalContainer from './common/HorizontalContainer';
 import DropDownMenu from './common/DropDownMenu';
-import book from './image/basic-book.jpg';
+import book1 from './image/book1.jpg';
+import book2 from './image/book2.jpg';
+import book3 from './image/book3.jpg';
+import book4 from './image/book4.jpg';
+import SlideMenu from './common/SlideMenu';
 import VerticalContainer from './common/VerticalContainer';
 
 const firstMenuDropDown = [
@@ -25,75 +27,7 @@ const thirdMenuDropDown = [
   '장르별게시판',
 ];
 
-const SlideImage = styled.img`
-  width:100%;
-  height:70vh;
-`;
-
-function SlideItem({ img }) {
-  return (
-    <SlideImage src={img || book} />
-  );
-}
-
-const SlideItemContainer = styled.div`
-  width:50%;
-  overflow: hidden;
-`;
-
-const SlideContainer = styled.div`
-  width: 100%;
-  display: flex; //이미지들을 가로로 나열합니다.
-`;
-
-const SlideMenuContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-
-function SlideMenu({ slideNumber }) {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  // 현재 보여주고 있는 슬라이드
-  const slideRef = useRef(0);
-
-  const nextSlide = () => {
-    if (currentSlide >= slideNumber) {
-      setCurrentSlide(() => 0);
-    } else {
-      setCurrentSlide((cur) => cur + 1);
-    }
-  };
-
-  const prevSlide = () => {
-    if (currentSlide === 0) {
-      setCurrentSlide(() => slideNumber);
-    } else {
-      setCurrentSlide((cur) => cur - 1);
-    }
-  };
-
-  useEffect(() => {
-    slideRef.current.style.transition = 'all 0.5s ease-in-out';
-    slideRef.current.style.transform = `translateX(-${currentSlide}00%)`;
-  }, [currentSlide]);
-
-  return (
-    <SlideMenuContainer>
-      <MdArrowBackIosNew onClick={prevSlide} />
-      <SlideItemContainer>
-        <SlideContainer ref={slideRef}>
-          <SlideItem />
-          <SlideItem />
-          <SlideItem />
-          <SlideItem />
-        </SlideContainer>
-      </SlideItemContainer>
-      <MdArrowForwardIos onClick={nextSlide} />
-    </SlideMenuContainer>
-  );
-}
+const slideMenuImageList = [book1, book2, book3, book4];
 
 function MainPage() {
   return (
@@ -104,7 +38,7 @@ function MainPage() {
         <DropDownMenu menuName="전체글" dropDownItemList={secondMenuDropDown} />
         <DropDownMenu menuName="전체글" dropDownItemList={thirdMenuDropDown} />
       </HorizontalContainer>
-      <SlideMenu slideNumber={3} />
+      <SlideMenu slideMenuImages={slideMenuImageList} />
     </VerticalContainer>
   );
 }
