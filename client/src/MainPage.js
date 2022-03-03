4e3071213ec6604dc7fcf2868ef6b34b4ca67122
witch-factory <soakdma37@gmail.com>
2022-03-04 00:37:31 +0900

    검색창 추가

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -88,7 +88,11 @@ function MainPage() {
 
       <PageHeader />
 
-      <SearchBox />
+      <SearchBox
+        mainPhrase="원하는 것을 검색해 보세요.
+        책, 서평, 소모임, 책에 관련된 모든 것이 있답니다."
+        subPhrase="원하는 게 딱히 없다면 추천해 드릴 수도 있어요."
+      />
       <SlideMenu slideMenuImages={slideMenuImageList} />
       <SmallGroupSummary TitleList={smallGroupTitleList} />
 
--- client/src/common/SearchBox.js
+++ client/src/common/SearchBox.js
@@ -57,16 +57,14 @@ function SearchBoxSearchButton() {
   );
 }
 
-function SearchBox() {
+function SearchBox({ mainPhrase, subPhrase }) {
   return (
     <SearchEngineContainer>
       <SearchEngineMainPhrase>
-        원하는 것을 검색해 보세요.
-        <br />
-        책, 서평, 소모임, 책에 관련된 모든 것이 있답니다.
+        {mainPhrase}
       </SearchEngineMainPhrase>
       <SearchEngineSubPhrase>
-        원하는 게 없다면 추천해 드릴 수도 있어요.
+        {subPhrase}
       </SearchEngineSubPhrase>
       <SearchBoxContainer>
         <SearchTextBox placeholder="검색하기" />
