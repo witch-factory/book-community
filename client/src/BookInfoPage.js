6291ab7f95b4c1d6e16f286e69dba6dbe5f13c5d
witch-factory <soakdma37@gmail.com>
2022-03-16 23:45:17 +0900

    책표지그림 삽입

--- client/src/BookInfoPage.js
+++ client/src/BookInfoPage.js
@@ -3,15 +3,18 @@ import VerticalContainer from './common/VerticalContainer';
 import PageHeader from './common/PageHeader';
 import SearchBox from './common/SearchBox';
 import PageFooter from './common/PageFooter';
+import book5 from './image/book5.jpg';
+
+function BookCoverImage({ coverImg }) {
+  return <img src={coverImg} alt="Book cover" />;
+}
 
 function BookInfoPage() {
   return (
     <VerticalContainer>
       <PageHeader />
-      <SearchBox
-        mainPhrase="당신 주변에 어떤 사람들이 있는지, 그리고 그 사람들이 읽은 이야기는 무엇인지
-        궁금하지 않나요?"
-      />
+      <SearchBox />
+      <BookCoverImage coverImg={book5} />
       <PageFooter />
     </VerticalContainer>
   );
