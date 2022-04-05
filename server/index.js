eb3f1536f919a52a4a5e213b43b507ed86688951
witch-factory <soakdma37@gmail.com>
2022-04-05 22:20:19 +0900

    free_board, free_board_comment, user_info 테이블 추가, 게시판 관련 아주 기본적인 api 작성

--- server/index.js
+++ server/index.js
@@ -4,12 +4,26 @@ import DBConnection from './db.js';
 const app = express();
 const port = 8000;
 
-app.get('/', async (req, res) => {
+app.get('/board/titles', async (req, res) => {
+  // 글들의 정보만 가져온다(댓글 제외)
   const pool = DBConnection();
   const conn = await pool.getConnection();
 
   try {
     const [rows] = await conn.query('select * from free_board');
+    res.send(rows);
+  } finally {
+    conn.release();
+  }
+});
+
+app.get('/board/letter', async (req, res) => {
+  // 글 하나를 댓글과 함께 가져온다
+  const pool = DBConnection();
+  const conn = await pool.getConnection();
+
+  try {
+    const [rows] = await conn.query('select * from free_board left outer join free_board_comment on free_board.id=free_board_comment.letter_id');
     res.send(rows);
   } finally {
     conn.release();
