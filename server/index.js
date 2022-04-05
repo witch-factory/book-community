e20030491d46279dde3fecca7ac4165f2295528a
witch-factory <soakdma37@gmail.com>
2022-04-05 23:01:11 +0900

    db 연동 완료. 기본  select * api 작성

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import cors from 'cors';
 import DBConnection from './db.js';
 
 const app = express();
 const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/user/info', async (req, res) => {
+  // 글들의 정보만 가져온다(댓글 제외)
+  const pool = DBConnection();
+  const conn = await pool.getConnection();
+
+  try {
+    const [rows] = await conn.query('select * from user_info');
+    res.send(rows);
+  } finally {
+    conn.release();
+  }
+});
 
 app.get('/board/titles', async (req, res) => {
   // 글들의 정보만 가져온다(댓글 제외)
